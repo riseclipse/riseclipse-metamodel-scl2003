b854033a09e8ebbb4a0c231e89ab8ce47111dd40
Dominique Marcadet <dominique.marcadet@centralesupelec.fr>
2021-10-14 13:56:08 +0200

    Update headers

--- fr.centralesupelec.edf.riseclipse.iec61850.scl.edit/src/fr/centralesupelec/edf/riseclipse/iec61850/scl/provider/ServiceYesNoItemProvider.java
+++ fr.centralesupelec.edf.riseclipse.iec61850.scl.edit/src/fr/centralesupelec/edf/riseclipse/iec61850/scl/provider/ServiceYesNoItemProvider.java
@@ -1,13 +1,13 @@
 /*
 *************************************************************************
-**  Copyright (c) 2019 CentraleSupélec & EDF.
+**  Copyright (c) 2016-2021 CentraleSupélec & EDF.
 **  All rights reserved. This program and the accompanying materials
 **  are made available under the terms of the Eclipse Public License v2.0
 **  which accompanies this distribution, and is available at
 **  https://www.eclipse.org/legal/epl-v20.html
-**
+** 
 **  This file is part of the RiseClipse tool
-**
+**  
 **  Contributors:
 **      Computer Science Department, CentraleSupélec
 **      EDF R&D
@@ -15,7 +15,7 @@
 **      dominique.marcadet@centralesupelec.fr
 **      aurelie.dehouck-neveu@edf.fr
 **  Web site:
-**      http://wdi.supelec.fr/software/RiseClipse/
+**      https://riseclipse.github.io/
 *************************************************************************
 */
 package fr.centralesupelec.edf.riseclipse.iec61850.scl.provider;
